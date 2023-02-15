5129f2485c0d3c485142ceca5f353ac2f5ff9609
usamaeltmsah <usamaeltmsah@gmail.com>
2023-02-15 06:15:08 +0200

    Initial Commit

---
+++ Bulbasaur/BulbasaurApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulbasaurApp.swift
+//  Bulbasaur
+//
+//  Created by Usama Fouad on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BulbasaurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
