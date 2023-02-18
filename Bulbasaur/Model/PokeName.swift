a336a57b2ce2582c89c4c6222c6c7bc652bb38a0
usamaeltmsah <usamaeltmsah@gmail.com>
2023-02-18 20:29:40 +0200

    Add all the 18 Pokémon names

---
+++ Bulbasaur/Model/PokeName.swift
@@ -0,0 +1,29 @@
+//
+//  PokeName.swift
+//  Bulbasaur
+//
+//  Created by Usama Fouad on 15/02/2023.
+//
+
+import Foundation
+
+enum PokeName: String {
+    case bug = "bug"
+    case dark = "dark"
+    case dragon = "dragon"
+    case electric = "electric"
+    case fairy = "fairy"
+    case fighting = "fighting"
+    case fire = "fire"
+    case flying = "flying"
+    case ghost = "ghost"
+    case grass = "grass"
+    case ground = "ground"
+    case ice = "ice"
+    case normal = "normal"
+    case poison = "poison"
+    case psychic = "psychic"
+    case rock = "rock"
+    case steel = "steel"
+    case water = "water"
+}
